3e02b9827b770314101fe534ee5a6de24a02cadf
Douglas Campos <qmx@qmx.me>
2011-05-09 02:36:18 -0300

    basic invokedynamic hello world (yay!)

---
+++ src/main/java/org/dynjs/Compiler.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dynjs;
+
+import java.lang.reflect.Method;
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodHandle;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import java.util.*;
+import org.objectweb.asm.*;
+
+/**
+ *
+ * @author qmx
+ */
+public class Compiler implements Opcodes {
+
+    public static byte[] compile(String name) {
+        ClassWriter cw = new ClassWriter(0);
+        FieldVisitor fv;
+        MethodVisitor mv;
+        AnnotationVisitor av0;
+        cw.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "invokedynamic", null, "java/lang/Object", null);
+        {
+            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+            mv.visitLdcInsn("Hello, weird invokedynamic!");
+            mv.visitInvokeDynamicInsn("print", "(Ljava/lang/String;)V", new MethodHandle(MH_INVOKESTATIC, "invokedynamic", "bootstrap", "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;"), new Object[]{});
+            mv.visitInsn(RETURN);
+            mv.visitMaxs(1, 1);
+            mv.visitEnd();
+        }
+        {
+            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "print", "(Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitInsn(SWAP);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V");
+            mv.visitInsn(RETURN);
+            mv.visitMaxs(2, 1);
+            mv.visitEnd();
+        }
+        {
+            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "bootstrap", "(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;", null, null);
+            mv.visitCode();
+            mv.visitTypeInsn(NEW, "java/lang/invoke/ConstantCallSite");
+            mv.visitInsn(DUP);
+            mv.visitVarInsn(ALOAD, 0);
+            mv.visitLdcInsn(Type.getType("Linvokedynamic;"));
+            mv.visitVarInsn(ALOAD, 1);
+            mv.visitVarInsn(ALOAD, 2);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/invoke/MethodHandles$Lookup", "findStatic", "(Ljava/lang/Class;Ljava/lang/String;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/MethodHandle;");
+            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/invoke/ConstantCallSite", "<init>", "(Ljava/lang/invoke/MethodHandle;)V");
+            mv.visitInsn(ARETURN);
+            mv.visitMaxs(6, 3);
+            mv.visitEnd();
+        }
+        cw.visitEnd();
+
+        return cw.toByteArray();
+    }
+
+    public static class DynamicClassLoader extends ClassLoader {
+
+        public Class<?> define(String className, byte[] bytecode) {
+            return super.defineClass(className, bytecode, 0, bytecode.length);
+        }
+    };
+
+    public static void main(String[] args) throws Exception {
+
+        DynamicClassLoader loader = new DynamicClassLoader();
+
+        Class<?> helloWorldClass = loader.define("invokedynamic",
+                compile("Test2"));
+
+        Method method = helloWorldClass.getMethod("main", String[].class);
+
+        method.invoke(
+                null, (Object) new String[]{});
+
+    }
+}
