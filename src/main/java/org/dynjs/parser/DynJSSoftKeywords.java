2182bbd03e1c552ecbaf018deb6848bce282cd9f
Douglas Campos <qmx@qmx.me>
2011-06-10 15:30:55 -0300

    adding license boilerplates

--- src/main/java/org/dynjs/parser/DynJSSoftKeywords.java
+++ src/main/java/org/dynjs/parser/DynJSSoftKeywords.java
@@ -1,11 +1,18 @@
-/*
- * $Id: 20811 2011-05-05 17:36:20Z porcelli $
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+/**
+ *  Copyright 2011 Douglas Campos <qmx@qmx.me>
+ *  Copyright 2011 Alexandre Porcelli <alexandre.porcelli@gmail.com>
  *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.dynjs.parser;
 
