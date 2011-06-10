e2f5e9ce0fee1cd0f2ec7704833b1061c7875da3
Douglas Campos <qmx@qmx.me>
2011-06-10 10:11:17 -0300

    cleanups

--- src/main/java/org/dynjs/parser/Executor.java
+++ src/main/java/org/dynjs/parser/Executor.java
@@ -16,33 +16,12 @@ import org.objectweb.asm.util.TraceClassVisitor;
 public class Executor implements Opcodes {
 
     public Statement printStatement(final Statement expression1) {
-        ClassWriter cw = new ClassWriter(0);
-        cw.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "invokedynamic", null, "java/lang/Object", null);
-        final MethodVisitor mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "print", "(Ljava/lang/String;)V", null, null);
-
-        Statement stmt = new Statement() {
-
-            @Override
-            public void reify(MethodVisitor mv) {
-                expression1.reify(mv);
-            }
-        };
-        stmt.reify(mv);
-        mv.visitEnd();
-        cw.visitEnd();
-        System.out.println(cw.toByteArray());
         
-        return stmt;
+        return null;
     }
 
     public Statement createLDC(final CommonTree stringLiteral) {
 
-        return new Statement() {
-
-            @Override
-            public void reify(MethodVisitor mv) {
-                mv.visitLdcInsn(stringLiteral.getText());
-            }
-        };
+        return null;
     }
 }
