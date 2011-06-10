fb682f537e6091a7244c60cc7f2691c2f9377040
Douglas Campos <qmx@qmx.me>
2011-06-10 12:00:03 -0300

    fmt & unused imports

--- src/test/java/org/dynjs/DynJsBootstrapTest.java
+++ src/test/java/org/dynjs/DynJsBootstrapTest.java
@@ -8,23 +8,14 @@ import java.lang.reflect.Method;
 import org.dynjs.Compiler.DynamicClassLoader;
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.BufferedTokenStream;
 import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.RecognitionException;
-import org.antlr.runtime.Token;
-import org.antlr.runtime.TokenSource;
-import org.antlr.runtime.TokenStream;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
 import org.dynjs.parser.ES3Lexer;
 import org.dynjs.parser.ES3Parser;
 import org.dynjs.parser.ES3Parser.program_return;
 import org.dynjs.parser.ES3Walker;
 import org.dynjs.parser.Executor;
-import org.dynjs.parser.Statement;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -38,7 +29,7 @@ public class DynJsBootstrapTest {
         CommonTokenStream stream = new CommonTokenStream(lexer);
         ES3Parser parser = new ES3Parser(stream);
         program_return program = parser.program();
-        CommonTree tree = (CommonTree)program.getTree();
+        CommonTree tree = (CommonTree) program.getTree();
         CommonTreeNodeStream treeNodeStream = new CommonTreeNodeStream(tree);
         ES3Walker walker = new ES3Walker(treeNodeStream);
         walker.setExecutor(new Executor());
