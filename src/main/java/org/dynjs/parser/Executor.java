9489e99d7b0f9945d5312ed0b67c2058d1129bd6
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2011-06-10 12:04:28 -0300

    removing copyrights

--- src/main/java/org/dynjs/parser/Executor.java
+++ src/main/java/org/dynjs/parser/Executor.java
@@ -1,12 +1,3 @@
-/*
- * $Id: 20811 2011-05-07 16:52:20Z porcelli $
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
 package org.dynjs.parser;
 
 import java.io.PrintWriter;
