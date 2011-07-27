a9692e98954d5825c273c38837b60eb3f2a88ca6
Douglas Campos <qmx@qmx.me>
2011-07-27 02:43:44 -0300

    going with fest assertions

--- src/test/java/org/dynjs/runtime/DynObjectTest.java
+++ src/test/java/org/dynjs/runtime/DynObjectTest.java
@@ -18,11 +18,8 @@ package org.dynjs.runtime;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.dynjs.runtime.helpers.hamcrest.IsUndefined.undefined;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.dynjs.runtime.helpers.fest.UndefinedCondition.undefined;
+import static org.fest.assertions.Assertions.assertThat;
 
 public class DynObjectTest {
 
@@ -35,14 +32,14 @@ public class DynObjectTest {
 
     @Test
     public void returnsUndefinedForUnknownAttributes() {
-        assertThat(object.get("prototype"), is(undefined()));
+        assertThat(object.get("prototype")).is(undefined());
     }
 
     @Test
     public void allowsSettingAttributes() {
         final DynObject person = new DynObject();
         object.set("person").to(person);
-        assertThat(object.get("person"), is(undefined()));
+        assertThat(object.get("person")).isNot(undefined());
     }
 
     @Test
@@ -51,6 +48,6 @@ public class DynObjectTest {
         final DynString blue = new DynString("blue");
         car.set("color").to(blue);
         object.set("prototype").to(car);
-        assertThat(object.get("color"), is(not(undefined())));
+        assertThat(object.get("color")).isNot(undefined());
     }
 }
