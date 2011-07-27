cf99442c4d147ff7b451d79ad36905dc112dd3e0
Douglas Campos <qmx@qmx.me>
2011-07-27 03:51:37 -0300

    testing attribute behavior

---
+++ src/test/java/org/dynjs/runtime/AttributesTest.java
@@ -0,0 +1,31 @@
+package org.dynjs.runtime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.dynjs.runtime.helpers.fest.UndefinedCondition.undefined;
+import static org.fest.assertions.Assertions.assertThat;
+
+
+public class AttributesTest {
+
+    private Attributes attributes;
+
+    @Before
+    public void setUp() throws Exception {
+        attributes = new Attributes();
+    }
+
+    @Test
+    public void neverReturnsNullValues() throws Exception {
+        assertThat(attributes.get("whatever")).is(undefined());
+    }
+
+    @Test
+    public void allowsAttributionOfValues(){
+        attributes.put("test", new Attribute<DynAtom>(new DynObject()));
+        assertThat(attributes.get("test"))
+                .isNot(undefined())
+                .isInstanceOf(Attribute.class);
+    }
+}
