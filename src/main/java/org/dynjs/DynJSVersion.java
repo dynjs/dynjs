1b9a268187fd8a5c6acf54730d237436adadc5a1
Plinio Balduino <pbalduino+github@gmail.com>
2011-09-06 22:12:33 -0300

    Added usage messages

---
+++ src/main/java/org/dynjs/DynJSVersion.java
@@ -0,0 +1,6 @@
+package org.dynjs;
+
+public class DynJSVersion {
+
+    public static final String FULL = "0.0.0 (development)";
+}
--- src/main/java/org/dynjs/Main.java
+++ src/main/java/org/dynjs/Main.java
@@ -1,5 +1,6 @@
 package org.dynjs;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,7 +13,30 @@ public class Main {
     private static void run(List<String> arguments) {
         if (arguments.isEmpty() || arguments.contains("--help")) {
             showUsage();
+        } else if (arguments.contains("--repl") || arguments.contains("--console")) {
+            startRepl();
+        } else if (arguments.contains("--version")) {
+            showVersion();
+        } else {
+            executeFile(arguments.get(0));
         }
+    }
+
+    private static void executeFile(String filename) {
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("File " + filename + " not found");
+        } else {
+            System.out.println("OK, you got me. Come back later please.");
+        }
+    }
+
+    private static void showVersion() {
+        System.out.println("Dyn.JS version " + DynJSVersion.FULL);
+    }
+
+    private static void startRepl() {
+        System.out.println("Not so fast Chuck");
     }
 
     private static void showUsage() {
@@ -24,8 +48,8 @@ public class Main {
                 .append("   --help      shows current screen with. Running without parameters also shows this.\n")
                 .append("   --repl      opens a REPL console to test small expressions.\n")
                 .append("   --version   shows current version.\n")
-                .append("\n")
-                .append("FILENAME is the file to execute. Duh.");
+                .append("   FILENAME    is the file to execute. Duh.\n")
+                .append("\n");
 
         System.out.println(usageText.toString());
     }
