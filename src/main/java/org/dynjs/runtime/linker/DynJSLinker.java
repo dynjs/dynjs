7fa50bbc36935e1b11e5928a29b917502cdf4f6b
Douglas Campos <qmx@qmx.me>
2011-09-10 03:44:30 -0300

    wiring initial type conversion + getProp calls

--- src/main/java/org/dynjs/runtime/linker/DynJSLinker.java
+++ src/main/java/org/dynjs/runtime/linker/DynJSLinker.java
@@ -3,17 +3,28 @@ package org.dynjs.runtime.linker;
 import org.dynalang.dynalink.linker.CallSiteDescriptor;
 import org.dynalang.dynalink.linker.GuardedInvocation;
 import org.dynalang.dynalink.linker.GuardingDynamicLinker;
+import org.dynalang.dynalink.linker.GuardingTypeConverterFactory;
 import org.dynalang.dynalink.linker.LinkRequest;
 import org.dynalang.dynalink.linker.LinkerServices;
-import org.dynalang.dynalink.support.Guards;
+import org.dynjs.api.Function;
+import org.dynjs.api.Scope;
+import org.dynjs.runtime.Converters;
 import org.dynjs.runtime.DynAtom;
+import org.dynjs.runtime.DynObject;
+import org.dynjs.runtime.DynString;
+import org.dynjs.runtime.DynThreadContext;
 import org.dynjs.runtime.RT;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
-public class DynJSLinker implements GuardingDynamicLinker {
+import static java.lang.invoke.MethodHandles.insertArguments;
+import static java.lang.invoke.MethodHandles.lookup;
+import static java.lang.invoke.MethodType.methodType;
+
+public class DynJSLinker implements GuardingDynamicLinker, GuardingTypeConverterFactory {
+
     @Override
     public GuardedInvocation getGuardedInvocation(LinkRequest linkRequest, LinkerServices linkerServices) throws Exception {
         CallSiteDescriptor callSiteDescriptor = linkRequest.getCallSiteDescriptor();
@@ -22,6 +33,17 @@ public class DynJSLinker implements GuardingDynamicLinker {
             MethodHandle print = lookup().findStatic(RT.class, "print", methodType);
 
             return new GuardedInvocation(print, null);
+        } else if (callSiteDescriptor.getName().startsWith("dyn:getProp")) {
+            MethodHandle getProperty = lookup().findVirtual(Scope.class, "resolve", methodType(DynAtom.class, String.class));
+            return new GuardedInvocation(getProperty, null);
+        }
+        return null;
+    }
+
+    @Override
+    public GuardedInvocation convertToType(Class<?> sourceType, Class<?> targetType) {
+        if(DynString.class.isAssignableFrom(sourceType) && String.class == targetType){
+            return Converters.Guarded_DynString2String;
         }
         return null;
     }
