10e9a766467a10573c96cca6f6e5d35199c81926
Douglas Campos <qmx@qmx.me>
2011-09-16 17:56:44 -0300

    adding add operation :)

---
+++ src/main/java/org/dynjs/runtime/DynNumber.java
@@ -0,0 +1,20 @@
+package org.dynjs.runtime;
+
+import org.dynjs.runtime.primitives.DynPrimitiveNumber;
+
+public class DynNumber extends DynObject {
+
+    private final double value;
+
+    public DynNumber(DynPrimitiveNumber primitiveNumber) {
+        this.value = primitiveNumber.getDoubleValue();
+    }
+
+    public DynNumber(double value) {
+        this.value = value;
+    }
+
+    public DynNumber add(DynNumber other) {
+        return new DynNumber(this.value + other.value);
+    }
+}
