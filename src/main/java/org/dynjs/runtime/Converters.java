965ae7bfbd2670f3b23fb34a328b442f333fb9db
Douglas Campos <qmx@qmx.me>
2011-09-16 10:50:03 -0300

    using dynalink lookup helper

--- src/main/java/org/dynjs/runtime/Converters.java
+++ src/main/java/org/dynjs/runtime/Converters.java
@@ -1,17 +1,15 @@
 package org.dynjs.runtime;
 
 import org.dynalang.dynalink.linker.GuardedInvocation;
-import org.dynalang.dynalink.support.Guards;
+import org.dynalang.dynalink.support.Lookup;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodType;
 
-import static java.lang.invoke.MethodHandles.lookup;
 import static java.lang.invoke.MethodType.methodType;
 
 public class Converters {
 
-    public static final MethodHandle DynString2String = findStatic(Converters.class, "convertDynString2String", methodType(String.class, Object.class));
+    public static final MethodHandle DynString2String = Lookup.PUBLIC.findStatic(Converters.class, "convertDynString2String", methodType(String.class, Object.class));
     public static final GuardedInvocation Guarded_DynString2String = new GuardedInvocation(DynString2String,
             null);
 
@@ -19,11 +17,4 @@ public class Converters {
         return dynString.toString();
     }
 
-    public static MethodHandle findStatic(Class cls, String name, MethodType type) {
-        try {
-            return lookup().findStatic(cls, name, type);
-        } catch (IllegalAccessException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
