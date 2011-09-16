2f6e65b50310da811ca039556c22bfcda26a1eea
Douglas Campos <qmx@qmx.me>
2011-09-16 17:36:52 -0300

    primitive number support

--- src/main/java/org/dynjs/runtime/primitives/DynPrimitiveNumber.java
+++ src/main/java/org/dynjs/runtime/primitives/DynPrimitiveNumber.java
@@ -3,6 +3,13 @@ package org.dynjs.runtime.primitives;
 import org.dynjs.runtime.DynAtom;
 
 public class DynPrimitiveNumber implements DynAtom {
+
+    private final String value;
+
+    public DynPrimitiveNumber(String value) {
+        this.value = value;
+    }
+
     @Override
     public boolean isPrimitive() {
         return true;
