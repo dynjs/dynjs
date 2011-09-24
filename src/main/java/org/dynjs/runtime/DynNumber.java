e70dff6199bd34fa09694f7d7601fec6a00b9a8e
Plinio Balduino <pbalduino+github@gmail.com>
2011-09-24 13:17:32 -0300

    Added some support to NaN

--- src/main/java/org/dynjs/runtime/DynNumber.java
+++ src/main/java/org/dynjs/runtime/DynNumber.java
@@ -2,8 +2,11 @@ package org.dynjs.runtime;
 
 import org.dynjs.runtime.primitives.DynPrimitiveNumber;
 
+import static java.lang.Double.*;
+
 public class DynNumber extends DynObject {
 
+    public static final DynNumber NAN = new DynNumber(NaN);
     private final double value;
 
     public DynNumber(DynPrimitiveNumber primitiveNumber) {
@@ -31,10 +34,21 @@ public class DynNumber extends DynObject {
     }
 
     public static DynNumber parseInt(final DynString string, final DynNumber radix) {
+        if(string.equals("")){
+            return NAN;
+        }
         return new DynNumber(0);
     }
 
     public double getValue() {
         return value;
+    }
+
+    public String toString(){
+        return isNaN() ? "NaN" : "" + getValue();
+    }
+
+    private boolean isNaN() {
+        return Double.isNaN(getValue());
     }
 }
--- src/test/java/org/dynjs/runtime/ParseIntTest.java
+++ src/test/java/org/dynjs/runtime/ParseIntTest.java
@@ -1,4 +1,16 @@
 package org.dynjs.runtime;
 
+import org.junit.Test;
+
+import static org.dynjs.runtime.DynNumber.NAN;
+import static org.dynjs.runtime.DynNumber.parseInt;
+import static org.fest.assertions.Assertions.assertThat;
+
 public class ParseIntTest {
+
+    @Test
+    public void emptyStringReturnsNaN(){
+        assertThat(parseInt(new DynString(""))).isEqualTo(NAN);
+    }
+
 }
