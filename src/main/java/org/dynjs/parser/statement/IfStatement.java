e4c4a17407ab2d89cfdffaf93a7500750994fd7e
Douglas Campos <qmx@qmx.me>
2011-09-25 23:33:46 -0300

    we have a working ifstatement :)

--- src/main/java/org/dynjs/parser/statement/IfStatement.java
+++ src/main/java/org/dynjs/parser/statement/IfStatement.java
@@ -2,6 +2,7 @@ package org.dynjs.parser.statement;
 
 import me.qmx.jitescript.CodeBlock;
 import org.dynjs.api.Function;
+import org.dynjs.api.Scope;
 import org.dynjs.parser.Statement;
 import org.dynjs.runtime.DynAtom;
 import org.dynjs.runtime.DynThreadContext;
@@ -30,10 +31,15 @@ public class IfStatement implements Statement {
         final FunctionStatement thenFn = new FunctionStatement(context, Collections.<String>emptyList(), this.vthen);
         final FunctionStatement elseFn = new FunctionStatement(context, Collections.<String>emptyList(), this.velse);
         CodeBlock codeBlock = CodeBlock.newCodeBlock()
-                .invokedynamic("dynjs:compile:if", sig(void.class, DynAtom.class, Function.class, Function.class), RT.BOOTSTRAP, RT.BOOTSTRAP_ARGS);
                 .append(vbool.getCodeBlock())
                 .append(thenFn.getCodeBlock())
                 .append(elseFn.getCodeBlock())
+                .invokedynamic("dynjs:compile:if", sig(Function.class, DynAtom.class, Function.class, Function.class), RT.BOOTSTRAP, RT.BOOTSTRAP_ARGS)
+                .aload(1)
+                .aload(2)
+                .aload(3)
+                .invokedynamic("dynjs:runtime:call", sig(DynAtom.class, Function.class, DynThreadContext.class, Scope.class, DynAtom[].class), RT.BOOTSTRAP, RT.BOOTSTRAP_ARGS)
+                .areturn();
         return codeBlock;
     }
 }
