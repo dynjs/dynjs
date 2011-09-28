ecd8b5d8430c5d44f2da853b8f259d4c23c7991c
Douglas Campos <qmx@qmx.me>
2011-09-28 02:54:29 -0300

    creating FunctionFactory

---
+++ src/main/java/org/dynjs/runtime/FunctionFactory.java
@@ -0,0 +1,50 @@
+package org.dynjs.runtime;
+
+import org.dynjs.api.Function;
+import org.dynjs.api.Scope;
+
+public class FunctionFactory implements Function {
+
+    private final Class<Function> clazz;
+
+    public FunctionFactory(Class<Function> clazz) {
+        this.clazz = clazz;
+    }
+
+    public static FunctionFactory create(Class<Function> clazz){
+        return new FunctionFactory(clazz);
+    }
+
+    @Override
+    public DynAtom call(DynThreadContext context, Scope scope, DynAtom[] arguments) {
+        return instantiate().call(context, scope, arguments);
+    }
+
+    private Function instantiate() {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public boolean isPrimitive() {
+        return false;
+    }
+
+    @Override
+    public Scope getEnclosingScope() {
+        return null;
+    }
+
+    @Override
+    public DynAtom resolve(String name) {
+        return null;
+    }
+
+    @Override
+    public void define(String property, DynAtom value) {
+    }
+}
