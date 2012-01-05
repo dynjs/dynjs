e204a9e68568ddabd9fe9bdf360a49a31a5c7e01
jaroslaw.palka@lumesse.com <jaroslaw.palka@lumesse.com>
2012-01-05 21:34:41 +0100

    moved DynJSEngine to top-level class

--- jsr223/src/main/java/org/dynjs/jsr223/DynJSEngineFactory.java
+++ jsr223/src/main/java/org/dynjs/jsr223/DynJSEngineFactory.java
@@ -2,22 +2,20 @@ package org.dynjs.jsr223;
 
 import static java.util.Arrays.asList;
 
-import java.io.Reader;
-import java.util.Arrays;
 import java.util.List;
 
-import javax.script.Bindings;
-import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
-import javax.script.ScriptException;
 
 public class DynJSEngineFactory implements ScriptEngineFactory {
+
+	private static final String ENGINE_NAME = "dynjs";
+	private static final List<String> ENGINE_NAMES = asList(ENGINE_NAME);
 
 	@Override
 	public String getEngineName() {
 		// TODO Auto-generated method stub
-		return "dynjs";
+		return ENGINE_NAME;
 	}
 
 	@Override
@@ -40,7 +38,7 @@ public class DynJSEngineFactory implements ScriptEngineFactory {
 
 	@Override
 	public List<String> getNames() {
-		return asList("dynjs");
+		return ENGINE_NAMES;
 	}
 
 	@Override
@@ -81,92 +79,7 @@ public class DynJSEngineFactory implements ScriptEngineFactory {
 
 	@Override
 	public ScriptEngine getScriptEngine() {
-		return new ScriptEngine() {
-			
-			@Override
-			public void setContext(ScriptContext context) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setBindings(Bindings bindings, int scope) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void put(String key, Object value) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public ScriptEngineFactory getFactory() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public ScriptContext getContext() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Bindings getBindings(int scope) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object get(String key) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object eval(Reader reader, Bindings n) throws ScriptException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object eval(String script, Bindings n) throws ScriptException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object eval(Reader reader, ScriptContext context) throws ScriptException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object eval(String script, ScriptContext context) throws ScriptException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object eval(Reader reader) throws ScriptException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Object eval(String script) throws ScriptException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Bindings createBindings() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		};
+		return new DynJSEngine();
 	}
 
 }
