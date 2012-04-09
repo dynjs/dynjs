b0cd01b76862ecb555f8b21af6dd464542714d9c
Bruno Oliveira <bruno@abstractj.com>
2012-04-09 20:44:57 -0300

    Including mock to test functions

--- dynjs/src/test/java/org/dynjs/runtime/DynJSTest.java
+++ dynjs/src/test/java/org/dynjs/runtime/DynJSTest.java
@@ -258,7 +258,7 @@ public class DynJSTest {
     }
 
     @Test
-    public void testBuiltinLoading() {
+    public void testMockFunctionLoading() {
         config.addBuiltin("sample", new MockFunction());
         check("var result = sample(true);");
     }
