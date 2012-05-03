e3614c55f9c32c8da8f1ebcb40f7e20611cd1a53
Douglas Campos <qmx@qmx.me>
2012-05-03 05:27:35 -0300

    1st stab at Object.defineProperty
    ugly, I know :P

--- src/main/java/org/dynjs/runtime/DynObject.java
+++ src/main/java/org/dynjs/runtime/DynObject.java
@@ -15,13 +15,13 @@
  */
 package org.dynjs.runtime;
 
+import org.dynjs.api.Callable;
+import org.dynjs.api.Function;
 import org.dynjs.api.Scope;
 import org.dynjs.exception.ReferenceError;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class DynObject implements Scope {
 
@@ -29,6 +29,40 @@ public class DynObject implements Scope {
 
     public DynObject() {
         setProperty("prototype", DynThreadContext.UNDEFINED);
+        setProperty("defineProperty", new Function() {
+            @Override
+            public Object call(DynThreadContext context, Object[] arguments) {
+                if (arguments.length == 3) {
+                    DynObject object = (DynObject) arguments[0];
+                    String name = (String) arguments[1];
+                    DynProperty property = new DynProperty(name);
+                    property.setAttribute("value", arguments[2]);
+                    object.setProperty(name, property);
+                    return object;
+                }
+                return DynThreadContext.UNDEFINED;
+            }
+
+            @Override
+            public void setContext(DynThreadContext context) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public Scope getEnclosingScope() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public Object resolve(String name) {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void define(String property, Object value) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
     }
 
     public void setProperty(String key, Object atom) {
