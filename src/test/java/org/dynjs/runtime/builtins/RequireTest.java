faf36875cb3ab6d455e1cc6b882e911118c9986b
Douglas Campos <qmx@qmx.me>
2012-06-06 05:52:37 -0300

    fixed nested requires
    - note that due to current changes on scope
     resolution you need to have an intermediate step
     for it to work

--- src/test/java/org/dynjs/runtime/builtins/RequireTest.java
+++ src/test/java/org/dynjs/runtime/builtins/RequireTest.java
@@ -89,7 +89,7 @@ public class RequireTest {
     
     @Test
     public void testSupportsNestedRequires() {
-    	check("var result = require('outer').quadruple(4);", 16);
+    	check("var x = require('outer'); var result = x.quadruple(4);", 16);
     }
     
     private void check(String scriptlet, Object expected) {
