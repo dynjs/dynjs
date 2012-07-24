2d1b8c24161dcc6ac3e2884ae95ce42197d4f22e
Douglas Campos <qmx@qmx.me>
2012-07-24 03:36:49 -0300

    naïve implementation of parseFloat

---
+++ src/main/java/org/dynjs/runtime/builtins/ParseFloat.java
@@ -0,0 +1,19 @@
+package org.dynjs.runtime.builtins;
+
+import org.dynjs.api.Function;
+import org.dynjs.runtime.DynThreadContext;
+
+public class ParseFloat implements Function {
+    @Override
+    public Object call(Object self, DynThreadContext context, Object... arguments) {
+        if (arguments.length == 1 && arguments[0] != null) {
+            return Double.parseDouble(arguments[0].toString());
+        }
+        return DynThreadContext.UNDEFINED;
+    }
+
+    @Override
+    public String[] getParameters() {
+        return new String[]{"a"};
+    }
+}
