0ac7aad2205eafcb05942e923aadd20f870321ca
Jingwen Owen Ou <jingweno@gmail.com>
2012-08-07 13:22:16 -0700

    Use wrapper type for ToNumber conversion

--- src/main/java/org/dynjs/runtime/conversion/ToNumber.java
+++ src/main/java/org/dynjs/runtime/conversion/ToNumber.java
@@ -3,11 +3,11 @@ package org.dynjs.runtime.conversion;
 
 // TODO: implement according to http://es5.github.com/#x9.3
 public class ToNumber {
-	public static double convert(String s) {
+	public static Double convert(String s) {
 		return s.isEmpty() ? 0.0 : Double.parseDouble(s);
 	}
 
-	public static double convert(Boolean b) {
+	public static Double convert(Boolean b) {
 		return b ? 1.0 : 0.0;
 	}
 }
