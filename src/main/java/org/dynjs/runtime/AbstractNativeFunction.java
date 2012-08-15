1c82033101ce737cc0fe379ae9f89c8fb3cc26d5
Bob McWhirter <bob@mcwhirter.org>
2012-08-15 17:15:25 -0400

    Fix in case we try making position arguments in strict=true mode (untested).

--- src/main/java/org/dynjs/runtime/AbstractNativeFunction.java
+++ src/main/java/org/dynjs/runtime/AbstractNativeFunction.java
@@ -21,27 +21,30 @@ public abstract class AbstractNativeFunction extends AbstractFunction {
     public List<VariableDeclaration> getVariableDeclarations() {
         return Collections.emptyList();
     }
-    
-    
+
     @Override
     public Object call(ExecutionContext context) {
         Reference selfRef = context.resolve( "this" );
         Object self = Types.UNDEFINED;
-        
-        if ( selfRef != null && ! selfRef.isUnresolvableReference() ) {
+
+        if (selfRef != null && !selfRef.isUnresolvableReference()) {
             self = selfRef.getValue( context );
         }
-        
+
         String[] formalParams = getFormalParameters();
-        Object[] args = new Object[ formalParams.length ];
-        for ( int i = 0 ; i < formalParams.length ; ++i ) {
+        Object[] args = new Object[formalParams.length];
+        for (int i = 0; i < formalParams.length; ++i) {
             Reference eachRef = context.resolve( formalParams[i] );
-            args[i] = eachRef.getValue( context );
+            if (!eachRef.isUnresolvableReference()) {
+                args[i] = eachRef.getValue( context );
+            } else {
+                args[i] = Types.UNDEFINED;
+            }
         }
-        
+
         return call( context, self, args );
     }
 
-    public abstract Object call(ExecutionContext context, Object self, Object...args);
+    public abstract Object call(ExecutionContext context, Object self, Object... args);
 
 }
