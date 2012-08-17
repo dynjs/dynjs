bba90399af90b6f40e873ff121fdc3582a77e02b
Bob McWhirter <bob@mcwhirter.org>
2012-08-17 09:00:03 -0400

    for( var i ... ) test.

---
+++ src/test/java/org/dynjs/runtime/ForVarDeclExpressionTest.java
@@ -0,0 +1,23 @@
+package org.dynjs.runtime;
+import static org.fest.assertions.Assertions.*;
+
+import org.junit.Test;
+
+
+public class ForVarDeclExpressionTest extends AbstractDynJSTestSupport {
+
+    @Test
+    public void testBasicLoop() {
+        eval( "var y=0;",
+                "for ( var i = 0 ; i < 10; ++i ) {",
+                "  y = i;",
+                "}" );
+        
+        Object i = getContext().resolve("i").getValue(getContext());
+        assertThat( i ).isEqualTo(10);
+        
+        Object y = getContext().resolve("y").getValue(getContext());
+        assertThat( y ).isEqualTo(9);
+        
+    }
+}
