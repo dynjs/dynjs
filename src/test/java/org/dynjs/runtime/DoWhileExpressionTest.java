1feb3ec87141ab0b0c1d02608d24236f38949bc8
Bob McWhirter <bob@mcwhirter.org>
2012-08-21 14:05:00 -0400

    Expect the expected exception.

--- src/test/java/org/dynjs/runtime/DoWhileExpressionTest.java
+++ src/test/java/org/dynjs/runtime/DoWhileExpressionTest.java
@@ -2,34 +2,40 @@ package org.dynjs.runtime;
 
 import static org.fest.assertions.Assertions.*;
 
+import org.dynjs.exception.ReferenceError;
 import org.junit.Test;
 
 public class DoWhileExpressionTest extends AbstractDynJSTestSupport {
-    
+
     @Test
     public void testBasicDoWhile() {
-        eval( "var x = 0",
+        eval("var x = 0",
                 "do {",
                 "  x = 42;",
                 "} while ( x != 42 );");
-        Object x = getContext().resolve( "x" ).getValue(getContext());
-        assertThat( x ).isEqualTo( 42 );
+        Object x = getContext().resolve("x").getValue(getContext());
+        assertThat(x).isEqualTo(42);
     }
-    
+
     @Test
     public void testDoWhileWithoutCurlies() {
-        eval( "var x = 0",
+        eval("var x = 0",
                 "do x = 42; while ( x != 42 );");
-        Object x = getContext().resolve( "x" ).getValue(getContext());
-        assertThat( x ).isEqualTo( 42 );
+        Object x = getContext().resolve("x").getValue(getContext());
+        assertThat(x).isEqualTo(42);
     }
-    
+
     @Test
     public void testDoWhileWithTestThrowing() {
-        eval( "var x = 0",
-                "do x = 42; while ( iDoNotExist );");
-        Object x = getContext().resolve( "x" ).getValue(getContext());
-        assertThat( x ).isEqualTo( 42 );
+        try {
+            eval("var x = 0",
+                    "do x = 42; while ( iDoNotExist );");
+            throw new AssertionError("Should have throw a ReferenceError on iDoNotExist");
+        } catch (ReferenceError e) {
+            // expected and correct
+        }
+        Object x = getContext().resolve("x").getValue(getContext());
+        assertThat(x).isEqualTo(42);
     }
 
 }
