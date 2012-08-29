3537b2c0736e9b096fa638fc5eb331071dbdde2f
Bob McWhirter <bob@mcwhirter.org>
2012-08-29 16:01:37 -0400

    Wire up the JSON.parse(str,reviver) to actually use the reviver function, if provided.

--- src/main/java/org/dynjs/runtime/builtins/types/json/Parse.java
+++ src/main/java/org/dynjs/runtime/builtins/types/json/Parse.java
@@ -1,6 +1,7 @@
 package org.dynjs.runtime.builtins.types.json;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParseException;
@@ -12,6 +13,9 @@ import org.dynjs.runtime.DynArray;
 import org.dynjs.runtime.DynObject;
 import org.dynjs.runtime.ExecutionContext;
 import org.dynjs.runtime.GlobalObject;
+import org.dynjs.runtime.JSFunction;
+import org.dynjs.runtime.JSObject;
+import org.dynjs.runtime.NameEnumerator;
 import org.dynjs.runtime.Types;
 import org.dynjs.runtime.builtins.types.BuiltinArray;
 import org.dynjs.runtime.builtins.types.BuiltinObject;
@@ -26,68 +30,114 @@ public class Parse extends AbstractNativeFunction {
     public Object call(ExecutionContext context, Object self, Object... args) {
         String jsonText = Types.toString(args[0]);
         JsonFactory factory = new JsonFactory();
-        Object result = null;
+        Object unfiltered = null;
         try {
             JsonParser parser = factory.createJsonParser(jsonText);
             parser.nextToken();
-            result = parse( context, parser );
+            unfiltered = parse(context, parser);
         } catch (IOException e) {
             throw new DynJSException(e);
         }
-        return result;
+
+        Object reviver = args[1];
+
+        if (!Types.isCallable(reviver)) {
+            return unfiltered;
+        }
+
+        JSObject root = BuiltinObject.newObject(context);
+        root.put(context, "", unfiltered, false);
+
+        return walk(context, (JSFunction) reviver, root, "");
     }
-    
+
+    protected Object walk(ExecutionContext context, JSFunction reviver, JSObject holder, String name) {
+        Object val = holder.get(context, name);
+
+        if (val instanceof JSObject) {
+            JSObject jsVal = (JSObject) val;
+            if (jsVal.getClassName().equals("Array")) {
+                int len = Types.toInteger(jsVal.get(context, "length"));
+                int i = 0;
+                while ( i < len ) {
+                    Object newElement = walk( context, reviver, jsVal, "" + i );
+                    if ( newElement == Types.UNDEFINED ) {
+                        jsVal.delete(context, "" + i, false );
+                    } else {
+                        jsVal.put(context, "" +i, newElement, false);
+                    }
+                    ++i;
+                }
+            } else {
+                NameEnumerator keys = jsVal.getOwnPropertyNames();
+                
+                while ( keys.hasNext() ) {
+                    String key = keys.next();
+                    Object newElement = walk( context, reviver, jsVal, key);
+                    if ( newElement == Types.UNDEFINED ) {
+                        jsVal.delete( context, key, false );
+                    } else {
+                        jsVal.put( context, key, newElement, false );
+                    }
+                }
+                
+            }
+        }
+        
+        return context.call(reviver, holder, name, val );
+    }
+
     protected Object parse(ExecutionContext context, JsonParser p) throws JsonParseException, IOException {
         JsonToken t = p.getCurrentToken();
-        if ( t == JsonToken.START_ARRAY ) {
-            return parseArray( context, p );
-        } else if ( t == JsonToken.START_OBJECT ) {
-            return parseObject( context, p );
+        if (t == JsonToken.START_ARRAY) {
+            return parseArray(context, p);
+        } else if (t == JsonToken.START_OBJECT) {
+            return parseObject(context, p);
         } else {
-            return parseValue( context, p );
-        }     
+            return parseValue(context, p);
+        }
     }
-    
+
     protected Object parseArray(ExecutionContext context, JsonParser p) throws JsonParseException, IOException {
         DynArray array = BuiltinArray.newArray(context);
         int i = 0;
-        while ( p.nextToken() != JsonToken.END_ARRAY ) {
-            Object value = parse( context, p );
-            array.put(context, ""+i, value, false);
+        while (p.nextToken() != JsonToken.END_ARRAY) {
+            Object value = parse(context, p);
+            array.put(context, "" + i, value, false);
             ++i;
         }
-        
+
         return array;
     }
-    
+
     protected Object parseObject(ExecutionContext context, JsonParser p) throws JsonParseException, IOException {
         DynObject obj = BuiltinObject.newObject(context);
-        
-        while ( p.nextToken() != JsonToken.END_OBJECT ) {
+
+        while (p.nextToken() != JsonToken.END_OBJECT) {
             String name = p.getCurrentName();
-            Object value = parse( context, p );
+            Object value = parse(context, p);
             obj.put(context, name, value, false);
         }
         return obj;
     }
-    
+
     protected Object parseValue(ExecutionContext context, JsonParser p) throws JsonParseException, IOException {
         JsonToken t = p.getCurrentToken();
-        
-        if ( t == JsonToken.VALUE_FALSE ) {
+
+        if (t == JsonToken.VALUE_FALSE) {
             return Boolean.FALSE;
-        } else if ( t == JsonToken.VALUE_TRUE ) {
+        } else if (t == JsonToken.VALUE_TRUE) {
             return Boolean.TRUE;
-        } else if ( t == JsonToken.VALUE_NULL ) {
+        } else if (t == JsonToken.VALUE_NULL) {
             return Types.NULL;
-        } else if ( t == JsonToken.VALUE_STRING ) {
+        } else if (t == JsonToken.VALUE_STRING) {
             return p.getText();
-        } else if ( t == JsonToken.VALUE_NUMBER_FLOAT ) {
+        } else if (t == JsonToken.VALUE_NUMBER_FLOAT) {
             return (double) p.getFloatValue();
-        } else if ( t == JsonToken.VALUE_NUMBER_INT ) {
+        } else if (t == JsonToken.VALUE_NUMBER_INT) {
             return p.getIntValue();
         }
-        
+
         return Types.NULL;
     }
 
--- src/test/java/org/dynjs/runtime/builtins/types/JSONTest.java
+++ src/test/java/org/dynjs/runtime/builtins/types/JSONTest.java
@@ -5,6 +5,7 @@ import static org.fest.assertions.Assertions.*;
 import org.dynjs.runtime.AbstractDynJSTestSupport;
 import org.dynjs.runtime.DynArray;
 import org.dynjs.runtime.JSObject;
+import org.dynjs.runtime.Types;
 import org.junit.Test;
 
 public class JSONTest extends AbstractDynJSTestSupport {
@@ -36,6 +37,30 @@ public class JSONTest extends AbstractDynJSTestSupport {
         
         assertThat( result.get( getContext(), "foo" )).isEqualTo(42);
         assertThat( result.get( getContext(), "bar" )).isEqualTo("cheese");
+    }
+    
+    @Test
+    public void testParseWithReviver() {
+        JSObject result = (JSObject) eval( "JSON.parse( '[1,2,3,4,5,6,7]', function(name,val){",
+                "if ( name != '' ) {",
+                "  if ( val % 2 == 0 ) {",
+                "    return undefined;",
+                "  } else {",
+                "    return val;",
+                "  }",
+                "} else {",
+                "  return val;",
+                "}",
+                "})");
+        
+        assertThat( result.get( getContext(), "length" ) ).isEqualTo(7);
+        assertThat( result.get( getContext(), "0" ) ).isEqualTo(1);
+        assertThat( result.get( getContext(), "1" ) ).isEqualTo(Types.UNDEFINED);
+        assertThat( result.get( getContext(), "2" ) ).isEqualTo(3);
+        assertThat( result.get( getContext(), "3" ) ).isEqualTo(Types.UNDEFINED);
+        assertThat( result.get( getContext(), "4" ) ).isEqualTo(5);
+        assertThat( result.get( getContext(), "5" ) ).isEqualTo(Types.UNDEFINED);
+        assertThat( result.get( getContext(), "6" ) ).isEqualTo(7);
     }
 
 }
