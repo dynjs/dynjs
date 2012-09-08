f1444e69b30a558fe5d70f0db8f7518ecd92c1f9
Douglas Campos <qmx@qmx.me>
2012-09-07 21:07:12 -0300

    DynDate should be treated as a primitive too

--- src/main/java/org/dynjs/runtime/builtins/types/date/DynDate.java
+++ src/main/java/org/dynjs/runtime/builtins/types/date/DynDate.java
@@ -1,9 +1,9 @@
 package org.dynjs.runtime.builtins.types.date;
 
-import org.dynjs.runtime.DynObject;
 import org.dynjs.runtime.GlobalObject;
+import org.dynjs.runtime.PrimitiveDynObject;
 
-public class DynDate extends DynObject {
+public class DynDate extends PrimitiveDynObject {
 
     public DynDate(GlobalObject globalObject) {
         super(globalObject);
