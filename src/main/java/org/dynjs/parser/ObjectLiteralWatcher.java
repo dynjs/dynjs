470355d6b584290a219e2243532628b15b75c058
Bob McWhirter <bob@mcwhirter.org>
2012-11-07 13:35:18 -0500

    Improvement on semantics of repeated property-assignments within object literals, even in non-strict mode.

--- src/main/java/org/dynjs/parser/ObjectLiteralWatcher.java
+++ src/main/java/org/dynjs/parser/ObjectLiteralWatcher.java
@@ -4,56 +4,49 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ObjectLiteralWatcher {
-    
+
     private boolean strict;
     private Set<String> values;
     private Set<String> getters;
     private Set<String> setters;
-    
+
     public ObjectLiteralWatcher(boolean strict) {
         this.strict = strict;
-        if ( strict ) {
-            this.values = new HashSet<>();
-            this.getters = new HashSet<>();
-            this.setters = new HashSet<>();
-        }
+        this.values = new HashSet<>();
+        this.getters = new HashSet<>();
+        this.setters = new HashSet<>();
     }
-    
+
     public boolean addValue(String name) {
-        if ( ! strict ) {
-            return true;
-        }
-        
-        if ( this.values.contains( name ) || this.setters.contains(name) || this.setters.contains(name)) {
+
+        if (this.getters.contains(name) || this.setters.contains(name)) {
             return false;
         }
-        this.values.add( name );
+
+        if (this.strict) {
+            if (this.values.contains(name)) {
+                return false;
+            }
+        }
+        this.values.add(name);
         return true;
     }
-    
+
     public boolean addSetter(String name) {
-        if ( ! strict ) {
-            return true;
-        }
-        
-        if ( this.values.contains(name) || this.setters.contains(name)) {
+        if (this.values.contains(name) || this.setters.contains(name)) {
             return false;
         }
-        
-        this.setters.add( name );
+
+        this.setters.add(name);
         return true;
     }
-    
+
     public boolean addGetter(String name) {
-        if ( ! strict ) {
-            return true;
-        }
-        
-        if ( this.values.contains(name) || this.getters.contains(name)) {
+        if (this.values.contains(name) || this.getters.contains(name)) {
             return false;
         }
-        
-        this.getters.add( name );
+
+        this.getters.add(name);
         return true;
     }
 
