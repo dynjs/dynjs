0a30ca2c918ee1f897cc614ca0702b786e52c8c1
Bob McWhirter <bob@mcwhirter.org>
2012-12-17 13:19:03 -0500

    Checkpoint interpretation of some statements/expressions.

--- src/main/java/org/dynjs/runtime/interp/InterpretingVisitor.java
+++ src/main/java/org/dynjs/runtime/interp/InterpretingVisitor.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.dynjs.exception.ThrowException;
 import org.dynjs.parser.CodeVisitor;
+import org.dynjs.parser.Statement;
 import org.dynjs.parser.ast.AdditiveExpression;
 import org.dynjs.parser.ast.ArrayLiteralExpression;
 import org.dynjs.parser.ast.AssignmentExpression;
@@ -49,6 +50,7 @@ import org.dynjs.parser.ast.ObjectLiteralExpression;
 import org.dynjs.parser.ast.PostOpExpression;
 import org.dynjs.parser.ast.PreOpExpression;
 import org.dynjs.parser.ast.PrintStatement;
+import org.dynjs.parser.ast.PropertyAssignment;
 import org.dynjs.parser.ast.PropertyGet;
 import org.dynjs.parser.ast.PropertySet;
 import org.dynjs.parser.ast.RegexpLiteralExpression;
@@ -69,12 +71,18 @@ import org.dynjs.parser.ast.VariableStatement;
 import org.dynjs.parser.ast.VoidOperatorExpression;
 import org.dynjs.parser.ast.WhileStatement;
 import org.dynjs.parser.ast.WithStatement;
+import org.dynjs.parser.js.Position;
 import org.dynjs.runtime.Completion;
 import org.dynjs.runtime.DynArray;
+import org.dynjs.runtime.DynObject;
+import org.dynjs.runtime.EnvironmentRecord;
 import org.dynjs.runtime.ExecutionContext;
+import org.dynjs.runtime.JSFunction;
 import org.dynjs.runtime.PropertyDescriptor;
+import org.dynjs.runtime.Reference;
 import org.dynjs.runtime.Types;
 import org.dynjs.runtime.builtins.types.BuiltinArray;
+import org.dynjs.runtime.builtins.types.BuiltinObject;
 
 public class InterpretingVisitor implements CodeVisitor {
 
@@ -117,9 +125,34 @@ public class InterpretingVisitor implements CodeVisitor {
     }
 
     @Override
-    public void visit(ExecutionContext context, BitwiseExpression bitwiseExpression, boolean strict) {
-        // TODO Auto-generated method stub
+    public void visit(ExecutionContext context, BitwiseExpression expr, boolean strict) {
+        expr.getLhs().accept(context, this, strict);
+        Object lhs = Types.getValue(context, pop());
 
+        expr.getRhs().accept(context, this, strict);
+        Long rhsNum = Types.toInt32(context, Types.getValue(context, pop()));
+
+        Long lhsNum = null;
+
+        if (expr.getOp().equals(">>>")) {
+            lhsNum = Types.toUint32(context, lhs);
+        } else {
+            lhsNum = Types.toInt32(context, lhs);
+        }
+
+        if (expr.getOp().equals("<<")) {
+            push(lhsNum.longValue() << rhsNum.intValue());
+        } else if (expr.getOp().equals(">>")) {
+            push(lhsNum.longValue() >> rhsNum.intValue());
+        } else if (expr.getOp().equals(">>>")) {
+            push(lhsNum.longValue() >>> rhsNum.intValue());
+        } else if (expr.getOp().equals("&")) {
+            push(lhsNum.longValue() & rhsNum.longValue());
+        } else if (expr.getOp().equals("|")) {
+            push(lhsNum.longValue() | rhsNum.longValue());
+        } else if (expr.getOp().equals("^")) {
+            push(lhsNum.longValue() ^ rhsNum.longValue());
+        }
     }
 
     @Override
@@ -138,19 +171,60 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, AssignmentExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
+        expr.getLhs().accept(context, this, strict);
+        Object lhs = pop();
+        if (!(lhs instanceof Reference)) {
+            throw new ThrowException(context, context.createTypeError(expr.getLhs() + " is not a reference"));
+        }
+
+        Reference lhsRef = (Reference) lhs;
+
+        expr.getRhs().accept(context, this, strict);
+        Object rhs = Types.getValue(context, pop());
+
+        lhsRef.putValue(context, rhs);
+        push(rhs);
     }
 
     @Override
     public void visit(ExecutionContext context, BitwiseInversionOperatorExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
-
+        expr.getExpr().accept(context, this, strict);
+        push(~Types.toInt32(context, Types.getValue(context, pop())));
     }
 
     @Override
     public void visit(ExecutionContext context, BlockStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
+        List<Statement> content = statement.getBlockContent();
 
+        Object completionValue = Types.UNDEFINED;
+
+        for (Statement each : content) {
+            Position position = each.getPosition();
+            if (position != null) {
+                context.setLineNumber(position.getLine());
+            }
+
+            each.accept(context, this, strict);
+            Completion completion = (Completion) pop();
+            if (completion.type == Completion.Type.NORMAL) {
+                if (completion.value != null) {
+                    completionValue = completion.value;
+                }
+                continue;
+            }
+            if (completion.type == Completion.Type.CONTINUE) {
+                continue;
+            }
+            if (completion.type == Completion.Type.RETURN) {
+                push(completion);
+                return;
+            }
+            if (completion.type == Completion.Type.BREAK) {
+                break;
+            }
+        }
+
+        push(Completion.createNormal(completionValue));
     }
 
     @Override
@@ -160,8 +234,7 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, BreakStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
-
+        push(Completion.createBreak(statement.getTarget()));
     }
 
     @Override
@@ -190,32 +263,65 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, ContinueStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
-
+        push(Completion.createContinue(statement.getTarget()));
     }
 
     @Override
     public void visit(ExecutionContext context, DeleteOpExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
+        expr.getExpr().accept(context, this, strict);
+        Object result = pop();
+        if (!(result instanceof Reference)) {
+            push(true);
+            return;
+        }
 
+        Reference ref = (Reference) result;
+        if (ref.isUnresolvableReference()) {
+            if (strict) {
+                throw new ThrowException(context, context.createSyntaxError("cannot delete unresolvable reference"));
+            } else {
+                push(true);
+                return;
+            }
+        }
+
+        if (ref.isPropertyReference()) {
+            push(Types.toObject(context, ref.getBase()).delete(context, ref.getReferencedName(), ref.isStrictReference()));
+            return;
+        }
+
+        if (ref.isStrictReference()) {
+            throw new ThrowException(context, context.createSyntaxError("cannot delete from environment record binding"));
+        }
+
+        EnvironmentRecord bindings = (EnvironmentRecord) ref.getBase();
+
+        push(bindings.deleteBinding(context, ref.getReferencedName()));
     }
 
     @Override
     public void visit(ExecutionContext context, DoWhileStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void visit(ExecutionContext context, EmptyStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
-
+        push(Completion.createNormal());
     }
 
     @Override
     public void visit(ExecutionContext context, EqualityOperatorExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
+        expr.getLhs().accept(context, this, strict);
+        expr.getRhs().accept(context, this, strict);
 
+        Object rhs = Types.getValue(context, pop());
+        Object lhs = Types.getValue(context, pop());
+
+        if (expr.getOp().equals("==")) {
+            push(Types.compareEquality(context, lhs, rhs));
+        } else {
+            push(!Types.compareEquality(context, lhs, rhs));
+        }
     }
 
     @Override
@@ -280,8 +386,16 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, IfStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
+        statement.getTest().accept(context, this, strict);
 
+        Boolean result = Types.toBoolean(pop());
+        if (result) {
+            statement.getThenBlock().accept(context, this, strict);
+        } else if (statement.getElseBlock() != null) {
+            statement.getElseBlock().accept(context, this, strict);
+        } else {
+            push(Completion.createNormal());
+        }
     }
 
     @Override
@@ -334,7 +448,7 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, NullLiteralExpression expr, boolean strict) {
-        push( Types.NULL );
+        push(Types.NULL);
     }
 
     @Override
@@ -344,8 +458,26 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, ObjectLiteralExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
+        DynObject obj = BuiltinObject.newObject(context);
 
+        List<PropertyAssignment> assignments = expr.getPropertyAssignments();
+
+        for (PropertyAssignment each : assignments) {
+            each.accept(context, this, strict);
+            Object value = Types.getValue(context, pop());
+            Object original = obj.getOwnProperty(context, each.getName());
+            PropertyDescriptor desc = null;
+            if (each instanceof PropertyGet) {
+                desc = PropertyDescriptor.newPropertyDescriptorForObjectInitializerGet(original, each.getName(), (JSFunction) value);
+            } else if (each instanceof PropertySet) {
+                desc = PropertyDescriptor.newPropertyDescriptorForObjectInitializerSet(original, each.getName(), (JSFunction) value);
+            } else {
+                desc = PropertyDescriptor.newPropertyDescriptorForObjectInitializer(each.getName(), value);
+            }
+            obj.defineOwnProperty(context, each.getName(), desc, false);
+        }
+
+        push(obj);
     }
 
     @Override
@@ -369,7 +501,6 @@ public class InterpretingVisitor implements CodeVisitor {
     @Override
     public void visit(ExecutionContext context, PropertyGet propertyGet, boolean strict) {
         // TODO Auto-generated method stub
-
     }
 
     @Override
@@ -380,8 +511,7 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, NamedValue namedValue, boolean strict) {
-        // TODO Auto-generated method stub
-
+        namedValue.getExpr().accept(context, this, strict);
     }
 
     @Override
@@ -398,11 +528,11 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, ReturnStatement statement, boolean strict) {
-        if ( statement.getExpr() != null ) {
+        if (statement.getExpr() != null) {
             statement.getExpr().accept(context, this, strict);
-            push( Completion.createReturn(pop()));
+            push(Completion.createReturn(pop()));
         } else {
-            push( Completion.createReturn(Types.UNDEFINED));
+            push(Completion.createReturn(Types.UNDEFINED));
         }
     }
 
@@ -424,23 +554,22 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, TernaryExpression expr, boolean strict) {
-        expr.getTest().accept( context, this, strict );
-        if ( Types.toBoolean( pop())) {
-            expr.getThenExpr().accept( context, this, strict );
+        expr.getTest().accept(context, this, strict);
+        if (Types.toBoolean(pop())) {
+            expr.getThenExpr().accept(context, this, strict);
         } else {
-            expr.getElseExpr().accept( context, this, strict );
+            expr.getElseExpr().accept(context, this, strict);
         }
     }
 
-
     @Override
     public void visit(ExecutionContext context, ThisExpression expr, boolean strict) {
-        push( context.getThisBinding() );
+        push(context.getThisBinding());
     }
 
     @Override
     public void visit(ExecutionContext context, ThrowStatement statement, boolean strict) {
-        statement.getExpr().accept( context, this, strict );
+        statement.getExpr().accept(context, this, strict);
         throw new ThrowException(context, pop());
     }
 
@@ -452,20 +581,29 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, TypeOfOpExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
-
+        expr.getExpr().accept(context, this, strict);
+        push(Types.typeof(context, pop()));
     }
 
     @Override
     public void visit(ExecutionContext context, UnaryMinusExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
-
+        expr.getExpr().accept(context, this, strict);
+        Number oldValue = Types.toNumber(context, pop());
+        if (oldValue instanceof Double) {
+            if (Double.isNaN(oldValue.doubleValue())) {
+                push(Double.NaN);
+            } else {
+                push(-1 * oldValue.doubleValue());
+            }
+        } else {
+            push(-1 * oldValue.longValue());
+        }
     }
 
     @Override
     public void visit(ExecutionContext context, UnaryPlusExpression expr, boolean strict) {
-        // TODO Auto-generated method stub
-
+        expr.getExpr().accept(context, this, strict);
+        push(Types.toNumber(context, pop()));
     }
 
     @Override
@@ -488,8 +626,46 @@ public class InterpretingVisitor implements CodeVisitor {
 
     @Override
     public void visit(ExecutionContext context, WhileStatement statement, boolean strict) {
-        // TODO Auto-generated method stub
+        Expression testExpr = statement.getTest();
+        Statement block = statement.getBlock();
 
+        Object v = null;
+
+        while (true) {
+            testExpr.accept(context, this, strict);
+            Boolean testResult = Types.toBoolean(Types.getValue(context, pop()));
+            if (testResult) {
+                block.accept(context, this, strict);
+                Completion completion = (Completion) pop();
+                if (completion.value != null) {
+                    v = completion.value;
+                }
+                if (completion.type == Completion.Type.CONTINUE) {
+                    if (completion.target == null) {
+                        continue;
+                    } else if (!statement.getLabels().contains(completion.target)) {
+                        push(completion);
+                        return;
+                    }
+                }
+                if (completion.type == Completion.Type.BREAK) {
+                    if (completion.target == null) {
+                        break;
+                    } else if (!statement.getLabels().contains(completion.target)) {
+                        push(completion);
+                        return;
+                    }
+                }
+                if (completion.type == Completion.Type.RETURN) {
+                    push(Completion.createReturn(v));
+                    return;
+                }
+            } else {
+                break;
+            }
+        }
+
+        push(Completion.createNormal(v));
     }
 
     @Override
