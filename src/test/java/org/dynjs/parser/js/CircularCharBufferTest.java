94db602868643baff9015769adda2d466da9ac66
Douglas Campos <qmx@qmx.me>
2013-03-19 00:34:19 -0300

    fix tyop

--- src/test/java/org/dynjs/parser/js/CircularCharBufferTest.java
+++ src/test/java/org/dynjs/parser/js/CircularCharBufferTest.java
@@ -8,7 +8,7 @@ import java.io.StringReader;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CiruclarCharBufferTest {
+public class CircularCharBufferTest {
     
     private StringReader reader;
     private CircularCharBuffer buf;
