d0acda16bbf93c1d1b5c696ad539e2b023a7a344
Lance Ball <lball@redhat.com>
2013-07-08 22:19:29 -0400

    Add tests for add/removeLoadPath

--- src/test/java/org/dynjs/runtime/builtins/RequireTest.java
+++ src/test/java/org/dynjs/runtime/builtins/RequireTest.java
@@ -4,6 +4,7 @@ import org.dynjs.runtime.AbstractDynJSTestSupport;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -16,4 +17,14 @@ public class RequireTest extends AbstractDynJSTestSupport {
     public void testRequirePaths() {
         assertThat(eval("require.paths")).isInstanceOf(ArrayList.class);
     }
+
+    @Test
+    public void testAddAndRemoveLoadPath() {
+        eval("require.addLoadPath('/foo')");
+        List<String> paths = (List<String>) eval("require.paths");
+        assertThat(paths.contains("/foo"));
+        eval("require.removeLoadPath('/foo')");
+        assertThat(!paths.contains("/foo"));
+    }
+
 }
