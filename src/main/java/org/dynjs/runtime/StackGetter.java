1ddd2d21c6026c59448ff45396e4589df04c20e5
Andrew Kiellor <akiellor@gmail.com>
2013-07-23 01:49:44 -0700

    Removed unused constructor, tidied fields.

--- src/main/java/org/dynjs/runtime/StackGetter.java
+++ src/main/java/org/dynjs/runtime/StackGetter.java
@@ -5,16 +5,7 @@ import java.util.List;
 
 public class StackGetter extends AbstractNativeFunction {
 
-    private String errorName;
-    private String message;
     private List<StackElement> stack;
-
-    public StackGetter(GlobalObject globalObject, String errorName, String message, List<StackElement> stack) {
-        super(globalObject, false);
-        this.errorName = errorName;
-        this.message = message;
-        this.stack = stack;
-    }
 
     public StackGetter(ExecutionContext context) {
         super(context.getGlobalObject());
@@ -26,21 +17,23 @@ public class StackGetter extends AbstractNativeFunction {
     @Override
     public Object call(ExecutionContext context, Object self, Object... args) {
         JSObject jsObject = (JSObject) self;
-        if (errorName == null && jsObject.hasProperty(context, "name")) {
+        String errorName = null;
+        if (jsObject.hasProperty(context, "name")) {
             errorName = Types.toString(context, jsObject.get(context, "name"));
         }
-        if (message == null && jsObject.hasProperty(context, "message")) {
+        String message = null;
+        if (jsObject.hasProperty(context, "message")) {
             message = Types.toString(context, jsObject.get(context, "message"));
         }
 
         StringBuilder buf = new StringBuilder();
-        if (this.errorName == null) {
+        if (errorName == null) {
             buf.append("<unknown>");
         } else {
-            buf.append(this.errorName);
+            buf.append(errorName);
         }
-        if (this.message != null && !this.message.equals("")) {
-            buf.append(": " + this.message);
+        if (message != null && !message.equals("")) {
+            buf.append(": " + message);
         }
         buf.append("\n");
         for (StackElement each : stack) {
