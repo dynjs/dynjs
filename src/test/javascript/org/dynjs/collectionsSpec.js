85cbe8d54dac96ea82f28ded5fbb0bc474892a3a
Lance Ball <lball@redhat.com>
2013-08-29 12:34:54 -0400

    Add disabled spec for http://jira.codehaus.org/browse/DYNJS-120

---
+++ src/test/javascript/org/dynjs/collectionsSpec.js
@@ -0,0 +1,14 @@
+describe("Java Collections objects", function() {
+
+  describe("java.util.List", function(){
+
+    xit("should act like a Javascript Array", function(){
+      var list = new java.util.ArrayList()
+      list.add("Foo");
+      expect(list.length).toBe(1);
+      expect(list[0]).toBe("Foo");
+    });
+
+  });
+
+});
