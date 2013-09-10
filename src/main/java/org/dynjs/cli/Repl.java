879fe11c244db1b344d21dd21a85121229fc722a
Lance Ball <lball@redhat.com>
2013-09-10 16:57:52 -0400

    Add logging to the REPL

--- src/main/java/org/dynjs/cli/Repl.java
+++ src/main/java/org/dynjs/cli/Repl.java
@@ -22,12 +22,10 @@ import org.jboss.aesh.console.ConsoleCallback;
 import org.jboss.aesh.console.ConsoleOutput;
 import org.jboss.aesh.console.Prompt;
 import org.jboss.aesh.console.reader.ConsoleInputSession;
+import org.jboss.aesh.console.settings.QuitHandler;
 import org.jboss.aesh.console.settings.Settings;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Repl {
 
@@ -42,16 +40,27 @@ public class Repl {
     private final String welcome;
     private final String prompt;
 
+    private PrintWriter log;
+
     public Repl(DynJS runtime, InputStream in, OutputStream out) {
         this(runtime, in, out, WELCOME_MESSAGE, PROMPT);
     }
 
     public Repl(DynJS runtime, InputStream in, OutputStream out, String welcome, String prompt) {
+        this(runtime, in, out, welcome, prompt, System.getProperty("user.dir") + "/dynjs.log");
+    }
+
+    public Repl(DynJS runtime, InputStream in, OutputStream out, String welcome, String prompt, String log) {
         this.prompt = prompt;
         this.welcome = welcome;
         this.runtime = runtime;
         this.out = out;
         this.in = new ConsoleInputSession(in).getExternalInputStream();
+        try {
+            this.log = new PrintWriter(log);
+        } catch (IOException e) {
+            System.err.println("Cannot create error log " + log);
+        }
     }
 
     public void run() {
@@ -61,6 +70,14 @@ public class Repl {
             consoleSettings.setInputStream(this.in);
             consoleSettings.setHistoryPersistent(false);
             consoleSettings.enableOperatorParser(false);
+            consoleSettings.setQuitHandler(new QuitHandler() {
+                @Override
+                public void quit() {
+                    if (log != null) {
+                        log.close();
+                    }
+                }
+            });
             final Console console = Console.getInstance();
             console.pushToStdOut(welcome);
             console.setPrompt(new Prompt(prompt));
@@ -68,20 +85,25 @@ public class Repl {
                 @Override
                 public int readConsoleOutput(ConsoleOutput consoleOutput) throws IOException {
                     String statement = consoleOutput.getBuffer();
+                    log.write(statement + "\n");
                     if (statement.equalsIgnoreCase("exit")) {
                         console.stop();
                         return 0;
                     } else {
                         try {
                             Object object = runtime.evaluate(statement);
+                            log.write(object.toString() + "\n");
                             console.pushToStdOut(object.toString() + "\n");
                         } catch (DynJSException e) {
                             console.pushToStdErr(e.getLocalizedMessage() + "\n");
+                            logException(e);
                         } catch (IncompatibleClassChangeError e) {
                             console.pushToStdErr("ERROR> " + e.getLocalizedMessage() + "\n");
                             console.pushToStdErr("Error parsing statement: " + statement + "\n");
+                            logException(e);
                         } catch (Exception e) {
                             e.printStackTrace(new PrintWriter(out));
+                            logException(e);
                         }
                     }
                     return 0;
@@ -92,5 +114,11 @@ public class Repl {
             e.printStackTrace();
         }
 
+    }
+
+    private void logException(Throwable e) {
+        log.write(e.getLocalizedMessage() + "\n");
+        e.printStackTrace(log);
+        log.write("\n");
     }
 }
