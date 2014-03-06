6d9e09703f6067856de7aaaa5c73e4999a157dfe
Douglas Campos <qmx@qmx.me>
2014-03-06 18:21:41 -0300

    all classpath access via `dynjs.classpath.push()`

--- src/main/java/org/dynjs/runtime/builtins/DynJSBuiltin.java
+++ src/main/java/org/dynjs/runtime/builtins/DynJSBuiltin.java
@@ -1,21 +1,37 @@
 package org.dynjs.runtime.builtins;
 
 import org.dynjs.runtime.DynJS;
+import org.dynjs.runtime.DynamicClassLoader;
 
 import java.net.MalformedURLException;
 
 public class DynJSBuiltin {
     private final DynJS runtime;
+    private final Classpath classpath;
 
     public DynJSBuiltin(DynJS runtime) {
         this.runtime = runtime;
+        classpath = new Classpath(runtime.getConfig().getClassLoader());
     }
 
     public Object[] getArgv() {
         return this.runtime.getConfig().getArgv();
     }
 
-    public void addClassPathEntry(String path) throws MalformedURLException {
-        this.runtime.getConfig().getClassLoader().append(path);
+    public Classpath getClasspath() {
+        return classpath;
     }
+
+    public static class Classpath {
+        private final DynamicClassLoader classLoader;
+
+        private Classpath(DynamicClassLoader classLoader) {
+            this.classLoader = classLoader;
+        }
+
+        public void push(String entry) throws MalformedURLException {
+            this.classLoader.append(entry);
+        }
+    }
+
 }
