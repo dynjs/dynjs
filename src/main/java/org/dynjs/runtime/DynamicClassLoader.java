7e80d0e70b9778cd012d666077ff40829d172f4f
Douglas Campos <qmx@qmx.me>
2014-03-06 18:19:55 -0300

    tyop

--- src/main/java/org/dynjs/runtime/DynamicClassLoader.java
+++ src/main/java/org/dynjs/runtime/DynamicClassLoader.java
@@ -48,7 +48,7 @@ public class DynamicClassLoader extends URLClassLoader {
             File f = new File(target);
             String path = target;
             if (f.exists() && f.isDirectory() && !path.endsWith("/")) {
-                // URLClassLoader requires that directores end with slashes
+                // URLClassLoader requires that directories end with slashes
                 path = path + "/";
             }
             return new URL("file", null, path);
