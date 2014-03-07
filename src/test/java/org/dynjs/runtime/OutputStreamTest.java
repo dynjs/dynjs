b03bf3e76d7e18f3d3cc5077fa3dedc723259ade
Douglas Campos <qmx@qmx.me>
2014-03-06 23:06:18 -0300

    fix formatting

--- src/test/java/org/dynjs/runtime/OutputStreamTest.java
+++ src/test/java/org/dynjs/runtime/OutputStreamTest.java
@@ -1,31 +1,32 @@
 package org.dynjs.runtime;
 
-import static org.fest.assertions.Assertions.*;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.io.IOException;
-
 import org.dynjs.Config;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class OutputStreamTest {
     @Test
     public void testOutputStream() throws UnsupportedEncodingException, IOException {
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      PrintStream ps = new PrintStream(bos);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(bos);
 
-      Config config = new Config();
-      config.setOutputStream(ps);
-      DynJS dynjs = new DynJS(config);
+        Config config = new Config();
+        config.setOutputStream(ps);
+        DynJS dynjs = new DynJS(config);
 
-      dynjs.evaluate("print(\"hello world\");");
+        dynjs.evaluate("print(\"hello world\");");
 
-      ps.flush();
-      bos.flush();
-      String result = bos.toString("UTF-8");
+        ps.flush();
+        bos.flush();
+        String result = bos.toString("UTF-8");
 
-      assertThat(result).isEqualTo("hello world\n");
+        assertThat(result).isEqualTo("hello world\n");
 
     }
 }
