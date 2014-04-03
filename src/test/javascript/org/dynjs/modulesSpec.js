a83fc168c775331f992f1b9b8a06075ae2f8c9f0
Bob McWhirter <bob@mcwhirter.org>
2014-04-03 10:40:21 -0400

    Remove bad copy/paste muck.

--- src/test/javascript/org/dynjs/modulesSpec.js
+++ src/test/javascript/org/dynjs/modulesSpec.js
@@ -9,17 +9,4 @@ describe("modules", function() {
     require('module-a.js');
   });
 
-/*
-  it("should handle properties named 'false'", function() {
-    expect(obj.false).toBe('a false thing');
-    expect(obj[false]).toBe('a false thing');
-    expect(obj.foo.false).toBe('foo false');
-    expect(obj.foo[false]).toBe('foo false');
-    var foobar = obj.bar();
-    expect(foobar.false).toBe('bar false');
-    expect(foobar[false]).toBe('bar false');
-    expect(obj.bar().false).toBe('bar false');
-  });
-  */
-
 });
