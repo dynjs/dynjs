42801e981bf1820fbae22431e955b5568b069ca6
Thomas E. Enebo <tom.enebo@gmail.com>
2014-04-12 09:27:56 -0500

    Add proper dump for return statement to print its expression argument in --ast

--- src/main/java/org/dynjs/parser/ast/ReturnStatement.java
+++ src/main/java/org/dynjs/parser/ast/ReturnStatement.java
@@ -48,4 +48,9 @@ public class ReturnStatement extends BaseStatement {
     public Object accept(Object context, CodeVisitor visitor, boolean strict) {
         return visitor.visit(context, this, strict);
     }
+
+    @Override
+    public String dump(String indent) {
+        return super.dump(indent) + (this.expr == null ? "" : this.expr.dump(indent + "  "));
+    }
 }
