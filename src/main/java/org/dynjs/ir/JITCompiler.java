acd0d707f6bf0d071c5b2f42a32404311f7e14ad
Douglas Campos <qmx@qmx.me>
2014-05-23 01:48:46 -0300

    introduce IR’s JITCompiler

---
+++ src/main/java/org/dynjs/ir/JITCompiler.java
@@ -0,0 +1,35 @@
+package org.dynjs.ir;
+
+import org.dynjs.runtime.JSFunction;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class JITCompiler {
+    private static final Executor compilationQueue = Executors.newFixedThreadPool(8, new ThreadFactory() {
+        private final AtomicInteger count = new AtomicInteger(1);
+
+        public Thread newThread(Runnable runnable) {
+            Thread thread = new Thread(runnable);
+            thread.setName("IR_JIT_Compiler-" + count.getAndIncrement());
+            thread.setDaemon(true);
+            return thread;
+        }
+    });
+
+    public static interface CompilerCallback {
+        public void done(JSFunction compiledFunction);
+    }
+
+    public void compile(IRJSFunction function, final CompilerCallback callback) {
+        compilationQueue.execute(new Runnable() {
+            @Override
+            public void run() {
+                // compile
+                callback.done(null);
+            }
+        });
+    }
+}
