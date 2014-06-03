f6ecc981a4f74a858dd7def2191a1e9cfee8c406
Douglas Campos <qmx@qmx.me>
2014-06-03 00:54:10 -0300

    store the synthetic method name and sigs somewhere

--- src/main/java/org/dynjs/ir/FunctionScope.java
+++ src/main/java/org/dynjs/ir/FunctionScope.java
@@ -5,6 +5,8 @@ package org.dynjs.ir;
 public class FunctionScope extends Scope {
     private String[] parameterNames;
     private String name;
+    private String syntheticMethodName;
+    private String syntheticSignature;
 
     public FunctionScope(Scope parent, String fileName, boolean isStrict, String[] parameterNames, String name) {
         super(parent, fileName, isStrict);
@@ -19,5 +21,21 @@ public class FunctionScope extends Scope {
 
     public String[] getParameterNames() {
         return parameterNames;
+    }
+
+    public void setSyntheticMethodName(String syntheticMethodName) {
+        this.syntheticMethodName = syntheticMethodName;
+    }
+
+    public String getSyntheticMethodName() {
+        return syntheticMethodName;
+    }
+
+    public void setSyntheticSignature(String syntheticSignature) {
+        this.syntheticSignature = syntheticSignature;
+    }
+
+    public String getSyntheticSignature() {
+        return syntheticSignature;
     }
 }
