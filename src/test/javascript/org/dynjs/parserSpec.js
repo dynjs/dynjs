89d6d950f053ba142076a85a92ac33b1099162e3
mlaporte <mlaporte@coveo.com>
2014-07-07 12:42:07 +0200

    Add jasmine spec test to validate fix.

--- src/test/javascript/org/dynjs/parserSpec.js
+++ src/test/javascript/org/dynjs/parserSpec.js
@@ -24,3 +24,48 @@ describe("DynJS Parser", function() {
     expect(obj.bar().false).toBe('bar false');
   });
 });
+
+describe("named function expressions", function () {
+  describe("in strict mode", function () {
+    "use strict";
+
+    it("should compile in function scope", function () {
+      var f = function ident(x) {
+        return x;
+      };
+
+      expect(f('foo')).toBe('foo');
+    });
+
+    it("should compile in block scope", function () {
+      if (true) {
+        var f = function ident(x) {
+          return x;
+        };
+
+        expect(f('foo')).toBe('foo');
+      }
+    });
+  });
+
+  describe("in sloppy mode", function () {
+    it("should compile in function scope", function () {
+      var f = function ident(x) {
+        return x;
+      };
+
+      expect(f('foo')).toBe('foo');
+    });
+
+    it("should compile in block scope", function () {
+      if (true) {
+        var f = function ident(x) {
+          return x;
+        };
+
+        expect(f('foo')).toBe('foo');
+      }
+    });
+  });
+});
+
