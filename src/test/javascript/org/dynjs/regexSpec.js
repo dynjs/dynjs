b6f8920b9e8aaba68e8afc69d522c5b2f988ff1f
mlaporte <mlaporte@coveo.com>
2015-01-08 09:38:21 +0100

    Fix indent following merge conflict.

--- src/test/javascript/org/dynjs/regexSpec.js
+++ src/test/javascript/org/dynjs/regexSpec.js
@@ -1,27 +1,27 @@
 describe("regular expressions", function() {
 
-describe("String.prototype.replace with regular expressions", function() {
-  it("should handle simple string replacements with functions", function() {
-    var x = ":res[content-length] :referrer[google.com]";
-    var y = x.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/, function(_, a, b, c, d) {
-      expect(_).toBe(":res[content-length]");
-      expect(a).toBe("res");
-      expect(b).toBe("content-length");
-      expect(c).toBe(0);
-      expect(d).toBe(":res[content-length] :referrer[google.com]");
-      return a + "." + b;
+  describe("String.prototype.replace with regular expressions", function() {
+    it("should handle simple string replacements with functions", function() {
+      var x = ":res[content-length] :referrer[google.com]";
+      var y = x.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/, function(_, a, b, c, d) {
+        expect(_).toBe(":res[content-length]");
+        expect(a).toBe("res");
+        expect(b).toBe("content-length");
+        expect(c).toBe(0);
+        expect(d).toBe(":res[content-length] :referrer[google.com]");
+        return a + "." + b;
+      });
+      expect(y).toBe("res.content-length :referrer[google.com]");
     });
-    expect(y).toBe("res.content-length :referrer[google.com]");
-  });
 
-  it("should handle global string replacements with functions", function() {
-    var x = ":remote-addr - - [:date] :res[content-length] - - :foo[bar] :referrer[google.com]";
-    var y = x.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function(_, a, b, c, d) {
-      return a + "." + b;
+    it("should handle global string replacements with functions", function() {
+      var x = ":remote-addr - - [:date] :res[content-length] - - :foo[bar] :referrer[google.com]";
+      var y = x.replace(/:([-\w]{2,})(?:\[([^\]]+)\])?/g, function(_, a, b, c, d) {
+        return a + "." + b;
+      });
+      print(y);
+      expect(y).toBe("remote-addr.undefined - - [date.undefined] res.content-length - - foo.bar referrer.google.com");
     });
-    print(y);
-    expect(y).toBe("remote-addr.undefined - - [date.undefined] res.content-length - - foo.bar referrer.google.com");
-  });
   });
 
   describe("multiline", function() {
